5f2d1d3393c76ba37e97fa1570cf23043d11ac5c
Lukáš Kollár <lukas.kollar@gmail.com>
2025-04-28 17:04:39 +0200

    stranka tipy na vylet

---
+++ components/TipCard.vue
@@ -0,0 +1,54 @@
+<script setup>
+	defineProps({
+		tip: Object
+	})
+</script>
+
+<template>
+    <Card class="tip-card">
+		<template #header>
+			<img class="photo" alt="" :src="tip.photo" />
+		</template>
+        <template #title>
+			<div class="title">{{ tip.title }}</div>
+		</template>
+		<template #subtitle>
+			<div class="subtitle">{{ tip.subtitle }}</div>
+		</template>
+        <template #content>
+            <div class="description">{{ tip.description }}</div>
+		</template>
+		<template #footer>
+			<section class="info">
+				<Tag severity="success" rounded icon="pi pi-directions" :value="tip.distance"></Tag>
+			</section>
+		</template>
+    </Card>
+</template>
+
+<style scoped>
+	.tip-card {
+		break-inside: avoid;
+		overflow: hidden;
+		margin-bottom: 1rem;
+
+		.photo {
+			width: 100%;
+		}
+		.title {
+			font-size: 1.7rem;
+			color: var(--color01);
+		}
+		.subtitle {
+			font-size: 1.1rem;
+		}
+		.description {
+			color: var(--color02);
+			text-align: justify;
+		}
+		.info {
+			display: flex;
+			gap: 1rem;
+		}
+	}
+</style>
--- pages/tips.vue
+++ pages/tips.vue
@@ -17,9 +17,42 @@
 </script>
 
 <template>
-	<div>
-		{{ tips.body.title }}
-	</div>
+	<section class="content">
+		<h1 class="title">{{ tips.body.title }}</h1>
+		<div class="description">{{ tips.body.description }}</div>
+		<a class="btn" :href="tips.body.listOfTipsLink.url" target="_blank">{{ tips.body.listOfTipsLink.title }}</a>
+		<section class="list">
+			<TipCard :tip="tip" v-for="tip in tips.body.list" :key="tip"/>
+		</section>
+	</section>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.content {
+		padding: 4rem;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+		.description {
+			text-align: justify;
+		}
+		.btn {
+			align-self: center;
+		}
+		.list {
+			columns: 1;
+			gap: 1rem;
+
+			@media(min-width: 1000px) {
+				columns: 2;
+			}
+			@media(min-width: 1400px) {
+				columns: 3;
+			}
+			@media(min-width: 1900px) {
+				columns: 4;
+			}
+		}
+	}
+</style>
