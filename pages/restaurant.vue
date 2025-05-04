685596a57c18f813c8de6e2bb1cc2c757461352d
Lukáš Kollár <lukas.kollar@gmail.com>
2025-05-04 10:06:57 +0200

    init

--- pages/restaurant.vue
+++ pages/restaurant.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 	const i18n = useI18n()
 	const { data: restaurant }  = await useAsyncData(() => queryCollection('content').path(`/${i18n.locale.value}/pages/restaurant`).first())
 
