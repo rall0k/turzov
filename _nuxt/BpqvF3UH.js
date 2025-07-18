395802afcc58e352112194393204fbbfab25a0fe
rall0k <rall0k@users.noreply.github.com>
2025-07-18 15:03:36 +0000

    deploy: 4211e1be79619ba1e3ce790f82c1f4c71ef852ee

---
+++ _nuxt/BpqvF3UH.js
@@ -0,0 +1,1 @@
+import t from"./DWLa-DDg.js";import"./Be1fzYNM.js";import"./XJvJlULa.js";import"./KQxUhwsc.js";import"./CDmloFeY.js";var m={name:"InputSwitch",extends:t,mounted:function(){console.warn("Deprecated since v4. Use ToggleSwitch component instead.")}};export{m as default};
